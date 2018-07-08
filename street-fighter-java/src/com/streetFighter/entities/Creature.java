214d1f2ae94e497c3408a87a76cb27a2ab7ea3b7
chrollo1 <40929644+chrollo1@users.noreply.github.com>
2018-07-08 02:17:12 -0400

    Revert "street-fighter-java"
    This reverts commit 64104465dfa6e56040879a830cfebf2b9b79cc1e.

--- street-fighter-java/src/com/streetFighter/entities/Creature.java
+++
@@ -1,9 +0,0 @@
-package com.streetFighter.entities;
-
-public abstract class Creature extends Entity {
-	
-	// every subclass of creature has an x,y
-	public Creature(float x, float y) {
-		super(x, y);
-	}
-}
